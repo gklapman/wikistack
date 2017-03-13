7b90a4d705008e0910518fa0915c6acd7199b999
Alyssa Ramsey <aly.m.ramsey@gmail.com>
2017-03-13 17:02:55 -0500

    user pages etc half finished

--- models/index.js
+++ models/index.js
@@ -4,34 +4,56 @@ var db = new Sequelize('postgres://localhost:5432/wikistack');
 
 let Page = db.define('page', {
     title: {
-
         type: Sequelize.STRING,
         allowNull: false
-    },
-    urlTitle: {
-        type: Sequelize.STRING,
-        allowNull: false
+        },
+        urlTitle: {
+            type: Sequelize.STRING,
+            allowNull: false,
 
-    },
-    content: {
-        type: Sequelize.TEXT,
-    },
-    status: {
-        type: Sequelize.ENUM('open', 'closed'),
-        allowNull: false
+        },
+        content: {
+            type: Sequelize.TEXT,
+        },
+        status: {
+            type: Sequelize.ENUM('open', 'closed'),
+            allowNull: false
 
+        },
+        date: {
+            type: Sequelize.DATE,
+            defaultValue: Sequelize.NOW
+        }
     },
-    date: {
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.NOW
-    }
-}, {
-    getterMethods: {
-        route: function() {
-            return "/wiki/" + this.urlTitle;
+            {
+        hooks: {
+            beforeValidate: function(page, options){
+                var pageTitle = page.title;
+                console.log(pageTitle)
+                if (pageTitle){
+                    var newPageTitle = pageTitle.split('').map(function(letter){
+                      if (letter === ' '){
+                        return "_";
+                      }
+                      else {
+                        return letter;
+                      }
+                    }).join('');
+                    page.urlTitle = newPageTitle;
+                } else {
+                    page.urlTitle = Math.random().toString(36).substring(2,7);
+                }
+            }   
+        },
+        getterMethods: {
+            route: function() {
+                return "/wiki/" + this.urlTitle;
+            }
+
         }
     }
-})
+   
+)
 
 let User = db.define('user', {
     name: {
@@ -50,6 +72,7 @@ let User = db.define('user', {
 })
 
 
+Page.belongsTo(User, { as: 'author' });
 
 
 
--- routes/wiki.js
+++ routes/wiki.js
@@ -1,25 +1,65 @@
 const express = require('express')
-const Page = require('../models').Page
-const User = require('../models').User
 const router = express.Router();
+var models = require('../models');
+var Page = models.Page; 
+var User = models.User; 
+
+router.post('/', function(req, res, next) {
+
+	User.findOrCreate({
+	  where: {
+	    name: req.body.name,
+	    email: req.body.email
+	  }
+	})
+	.then(function (values) {
+
+	  var user = values[0];
+
+	  var page = Page.build({
+	    title: req.body.title,
+	    content: req.body.content,
+	    status: req.body.status
+	  });
+
+	  return page.save().then(function (page) {
+	    return page.setAuthor(user);
+	  });
+
+	})
+	.then(function (page) {
+	  res.redirect(page.route);
+	})
+	.catch(next);
+
+
+});
+
+
 
 router.get('/', (req, res, next) => {
-
+	//console.log("HI!!!!!!!")
     // Page.find()
-
-   
-    res.redirect('/');
+    var allThePages = Page.findAll();
+   //console.log("ALL THE PAGES", allThePages);
+    allThePages
+   	.then(allPages => {
+    	res.render('index', { allPages: allPages});
+    })
+    .catch(function(err){
+    	console.error(err);
+    })
 
 
     // res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true });
 });
 
 
-router.post('/', (req, res, next) => {
-    res.send('got to POST/wiki');
+// router.post('/', (req, res, next) => {
+//     res.json(req.body);
 
 
-});
+// });
 
 router.get('/add', (req, res, next) => {
     res.render('addpage', { title: 'wikistack.js' })
@@ -27,5 +67,32 @@ router.get('/add', (req, res, next) => {
 
 });
 
+router.get('/:urlTitle', function (req, res, next) {
+
+  Page.findOne({ 
+    where: { 
+      urlTitle: req.params.urlTitle 
+    } 
+
+  })
+  .then(function(foundPage){
+  	//console.log("FOUND PAGE IS:", foundPage);
+    // res.json(foundPage);
+    res.render('wikipage', {foundPage : foundPage})
+  })
+  .catch(next);
+
+});
+
 
 module.exports = router;
+
+
+// {"route":"/wiki/DOGS","id":1,"title":"DOGS","urlTitle":"DOGS","content":"DOGS","status":"closed","date":"2017-03-13T20:42:24.000Z","createdAt":"2017-03-13T20:42:24.719Z","updatedAt":"2017-03-13T20:42:24.719Z"}
+
+
+
+
+
+
+
